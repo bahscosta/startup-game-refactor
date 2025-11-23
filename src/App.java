51eaaa234d51bf8f94ced89b6e865da308bd8dba
Bárbara Silva Costa <10438935@mackenzista.com.br>
2025-11-23 01:08:51 -0300

    implementei o método iniciarSimulacao em App.java e inseri na classe a mensagem de saída incial.

--- src/App.java
+++ src/App.java
@@ -1,13 +1,38 @@
+//é a porta de entrada do jogo
+
 import config.Config;
 import engine.GameEngine;
 
 
 public class App {
-    public static void main (String[] args) {
-        Config config = new Config();
 
+    public static void main (String[] args) {
+        System.out.println("======================================");
+        System.out.println("      🚀 BEM-VINDO AO STARTUP GAME 🚀 ");
+        System.out.println("======================================");
+
+
+ 
+        //carrega as configurações do jogo (total de rodadas, decisões por rodada)
+        Config config = new Config();
+        System.out.println("Configurações carregadas:");
+        System.out.println(" - Total de rodadas: " + config.totalRodadas());
+        System.out.println(" - Máx. decisões por rodada: " + config.maxDecisoesPorRodada());
+        System.out.println("--------------------------------------");
+
+
+        //cria o motor do jogo - GameEngine
         GameEngine engine = new GameEngine(config);
 
+
+        //inicia a simulação:
         engine.iniciarSimulacaoStartup();
+
+
+    
+
+        System.out.println("\nObrigada por jogar o Startup Game ✨");
+        System.out.println("Fim do jogo. Volte sempre!");
     }
 }
+
